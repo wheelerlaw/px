f443f5747fd0e78ebb087d1f3659c6c651d61c07
Wheeler Law <wheeler@redhat.com>
2019-04-24 18:59:28 -0400

    Add https transport to apt-get

--- build.docker.sh
+++ build.docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -v $(pwd):/px --rm -it ubuntu bash -c '
-apt-get update && apt-get install -y curl software-properties-common gnupg1 unzip git
+apt-get update && apt-get install -y curl software-properties-common gnupg1 unzip git apt-transport-https ca-certificates
 curl -fsSL https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
 dpkg --add-architecture i386
 apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main"
